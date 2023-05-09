3c3839bdf3e981759ec559ec69d90821590307b9
ROBERTO MARTINEZ AVENDAÑO <romaav@floridauniversitaria.es>
2023-05-09 21:13:42 +0200

    actualizacion bbdd

--- Inserciones.sql
+++ Inserciones.sql
@@ -1,1 +1,1 @@
-INSERT INTO users VALUES (0,"CranioLocoLoquendero69","bikinisOnTop","supercarlitos69@gmail.com",NULL);
+INSERT INTO users VALUES (0,"SerenaLoquendera","bababa","serenadiaz@gmail.com",NULL);
--- Memigo_BBDD.sql
+++ Memigo_BBDD.sql
@@ -3,30 +3,29 @@ USE memigo_v1;
 
 CREATE TABLE users(
 	idUser INT NOT NULL AUTO_INCREMENT,
-	nameUser VARCHAR(50) NOT NULL,
-	passUser VARCHAR(200) NOT NULL,
+	username VARCHAR(50) NOT NULL,
+	userpass VARCHAR(200) NOT NULL,
 	email VARCHAR(200) NOT NULL,
-	imgUser MEDIUMTEXT,
+	userimg MEDIUMTEXT,
 	PRIMARY KEY(idUser)
 );
 
-CREATE TABLE templates(
-	idTemplate INT NOT NULL AUTO_INCREMENT,
-	imgTemplate MEDIUMTEXT NOT NULL,
-	PRIMARY KEY(idTemplate)  
-);
+#CREATE TABLE templates(
+#	idTemplate INT NOT NULL AUTO_INCREMENT,
+#	imgTemplate MEDIUMTEXT NOT NULL,
+#	PRIMARY KEY(idTemplate)  
+#);
 
 CREATE TABLE memes(
 	idMeme INT NOT NULL AUTO_INCREMENT,
-	idTemplate INT,
 	idUser INT NOT NULL,
-	imgMeme MEDIUMTEXT NOT NULL,
+	memeImg MEDIUMTEXT NOT NULL,
 	likes INT NOT NULL DEFAULT 0,
 	PRIMARY KEY(idMeme) 
 ); 
 
 ALTER TABLE memes ADD CONSTRAINT fk_memes_usu FOREIGN KEY 
-(idUser) REFERENCES users (idUser);
+(idUser) REFERENCES users (idUser) ON DELETE CASCADE;
 
-ALTER TABLE memes ADD CONSTRAINT fk_memes_temp FOREIGN KEY 
-(idTemplate) REFERENCES templates (idTemplate);
+#ALTER TABLE memes ADD CONSTRAINT fk_memes_temp FOREIGN KEY 
+#(idTemplate) REFERENCES templates (idTemplate);
