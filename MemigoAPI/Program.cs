f11efaca999bdc05e0d745211219ee6c58cfb389
ROBERTO MARTINEZ AVENDAÑO <romaav@floridauniversitaria.es>
2023-05-13 23:45:29 +0200

    politica CORS añadida

--- MemigoAPI/Program.cs
+++ MemigoAPI/Program.cs
@@ -3,6 +3,12 @@ using MemigoAPI.Model;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<MemigoV1Context>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyCors", builder => {
+        builder.AllowAnyOrigin().AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseAuthorization();
